09bbf37e9f0a51ee5b57d3e37ff57f49637a03b6
ZhengyuanY <diverf11@gmail.com>
2024-06-29 16:22:40 -0700

    add calling google main page for /g/

--- main.go
+++ main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func DefaultRootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, server UP!")
+}
+
+func CallExtServiceHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("https://www.google.com/ncr")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close() // Ensure the response body is closed
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Fprintln(w, string(body))
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", DefaultRootHandler)
+	http.HandleFunc("/g", CallExtServiceHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
